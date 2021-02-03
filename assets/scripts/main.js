9db07488dd0d4b55fc3d4769fcc3a0a84003904f
leeorn <padiii@gmail.com>
2021-02-03 06:48:22 -0800

    start working pn part 1 - slider and string val

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,42 @@
-// main.js
+// // main.js
 
-// TODO
+// globals
+let volSlider = document.getElementById("volume-slider");
+let volStr = document.getElementById("volume-number");
+let sound = document.getElementById("horn-sound") 
+
+
+/*
+TODO
+- when the slider changes change tne string vale
+- when the string value changes change the bar
+- 
+- 
+- 
+- 
+*/
+
+function updateVol(){
+
+}
+
+// when the slider changes, change the string value
+function changeStrVal(){
+    // change the string
+    volStr.value = volSlider.value;
+    // adjust the volume
+    sound.volume = volSlider.value / 100;
+    updateVol();
+}
+// when the string value changes, change the bar
+function changeSliderVal(){
+    volSlider.value = volStr.value;
+    sound.volume = volSlider.value / 100;
+    updateVol();
+}
+
+
+
+// events - when something changes it should trigger these
+volSlider.addEventListener("input", changeStrVal);
+volStr.addEventListener("input", changeSliderVal);
